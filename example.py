fd82edcd1f1694927b3d01b1e92da7414f6c162d
Sorch <woloinme@gmail.com>
2013-03-04 01:20:44 +0000

    added a dice command :)

--- example.py
+++ example.py
@@ -2,6 +2,7 @@
 rooms = ["room", "roomhere"]
 
 from irc import IRCBot, run_bot
+import random
 
 owner = "theholder" # put your irc nick here
 
@@ -13,10 +14,13 @@ class bot(IRCBot):
 		else:
 			ret = message[4:]
 			return ret
+	def dice(self, nick, message, channel):
+		return "%s rolls a dice and gets %s" % (nick, random.randrange(1, 6))
 		
 	def command_patterns(self):
 		return(
 			self.command("say", self.say),
+			self.command("dice", self.dice),
 		)
 		
 host = "irc.domain.tld"
